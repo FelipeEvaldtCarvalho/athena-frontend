559f1a12a40ef4889582f7393adc85569306d2d3
Felipe Evaldt Carvalho <felipe.carvalho@appmax.com.br>
2022-08-15 10:14:15 -0300

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>about</h1>
+    <div v-for="product in products" :key="product.id">
+      <h4>{{ product.name }}</h4>
+      <h4>{{ product.price }}</h4>
+      <h4>{{ product.category_id }}</h4>
+      <h4>{{ product.description }}</h4>
+      <hr>
+    </div>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'AboutComponent',
+  data () {
+    return{
+      products: {}
+    }
+  },
+  mounted() {
+    this.getProducts();
+  },
+  methods: {
+    getProducts(){
+        axios.get('http://localhost/api/products')
+        .then(response => {this.products = response.data.data ;})
+        .catch(error => console.log(error));
+    },
+  },
+
+}
+</script>
+
+<style>
+
+</style>
